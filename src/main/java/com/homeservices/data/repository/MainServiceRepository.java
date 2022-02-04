7e131dbb0f27b572b4469a0ef4af2e99908fb07b
zshahabi <zshahabyeng@gmail.com>
2022-02-03 22:57:16 -0800

    add main service

--- src/main/java/com/homeservices/data/repository/MainServiceRepository.java
+++ src/main/java/com/homeservices/data/repository/MainServiceRepository.java
@@ -1,5 +1,6 @@
 package com.homeservices.data.repository;
 
+import com.homeservices.Main;
 import com.homeservices.data.entity.MainService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MainServiceRepository extends JpaRepository<MainService, Long>
 {
+    MainService findByName(final String name);
 }
--- src/main/java/com/homeservices/service/MainServicesService.java
+++ src/main/java/com/homeservices/service/MainServicesService.java
@@ -2,6 +2,7 @@ package com.homeservices.service;
 
 import com.homeservices.data.entity.MainService;
 import com.homeservices.data.repository.MainServiceRepository;
+import com.homeservices.exception.FoundMainServiceException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,6 +35,20 @@ public record MainServicesService(MainServiceRepository repository)
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public boolean addCustomMainService(final String name) throws FoundMainServiceException
+    {
+        final MainService byName = repository.findByName(name);
+        if (byName == null)
+        {
+            final MainService mainService = new MainService();
+            mainService.setName(name);
+            repository.save(mainService);
+
+            return true;
+        }
+        else throw new FoundMainServiceException(name);
     }
 
     public List<MainService> mainServices()
